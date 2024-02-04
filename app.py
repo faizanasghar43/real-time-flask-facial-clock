b5c18e18756739ffad9da7ed6ac9433169246601
Faizan <taha.rasheed@zweidevs.com>
2024-02-04 23:30:03 +0500

    added docker files and port settings

--- app.py
+++ app.py
@@ -1,3 +1,112 @@
+# from flask import Flask, render_template, Response
+# from flask_socketio import SocketIO, emit
+# import face_recognition
+# import cv2
+# import pandas as pd
+# import numpy as np
+# import os
+# import datetime
+#
+# app = Flask(__name__)
+# socketio = SocketIO(app)
+#
+# video = cv2.VideoCapture(0)  # Initialize your video capture device
+#
+# csv_file = 'known_faces.csv'
+# df = pd.read_csv(csv_file) if os.path.exists(csv_file) else pd.DataFrame(
+#     columns=['username', 'face_encoding', 'last_seen'])
+#
+#
+# def save_known_faces():
+#     df.to_csv(csv_file, index=False)
+#
+#
+# def gen_frame():
+#     global df
+#     visible_faces = set()  # Set to keep track of faces that are currently visible
+#
+#     while True:
+#         _, frame = video.read()
+#         face_locations = face_recognition.face_locations(frame)
+#         face_encodings = face_recognition.face_encodings(frame, face_locations)
+#         current_visible_faces = set()  # Set to keep track of faces visible in the current frame
+#
+#         for face_encoding, face_location in zip(face_encodings, face_locations):
+#             if not df.empty:
+#                 face_encodings_as_arrays = [np.array(eval(encoding)) for encoding in df['face_encoding'].tolist()]
+#                 distances = face_recognition.face_distance(face_encodings_as_arrays, face_encoding)
+#                 best_match_index = np.argmin(distances) if len(distances) > 0 else None
+#
+#                 if best_match_index is not None and distances[best_match_index] <= 0.6:
+#                     username = df.at[best_match_index, 'username']
+#                     current_visible_faces.add(username)  # Add to current visible faces
+#
+#                     if username not in visible_faces:
+#                         visible_faces.add(username)  # Add to visible faces
+#
+#                     top, right, bottom, left = face_location
+#                     cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+#                     font = cv2.FONT_HERSHEY_DUPLEX
+#                     cv2.putText(frame, f'username: {username}', (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+#                 else:
+#                     # New face logic...
+#                     new_face_id = f"face_{len(df) + 1}"
+#                     current_time_str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#                     new_row = pd.DataFrame({'username': [new_face_id], 'face_encoding': [str(face_encoding.tolist())],
+#                                             'last_seen': [current_time_str]})
+#                     df = pd.concat([df, new_row], ignore_index=True)
+#                     save_known_faces()  # Save after adding a new face
+#                     socketio.emit('last_seen_update', {
+#                         'username': new_face_id,
+#                         'last_seen': current_time_str
+#                     })
+#             else:
+#                 # First face logic...
+#                 new_face_id = f"face_1"
+#                 current_time_str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#                 df = pd.DataFrame({'username': [new_face_id], 'face_encoding': [str(face_encoding.tolist())],
+#                                    'last_seen': [current_time_str]})
+#                 save_known_faces()  # Save after adding the first face
+#                 socketio.emit('last_seen_update', {
+#                     'username': new_face_id,
+#                     'last_seen': current_time_str
+#                 })
+#
+#         # Update last_seen for faces that have left the camera's view
+#         faces_no_longer_visible = visible_faces - current_visible_faces
+#         if faces_no_longer_visible:
+#             for username in faces_no_longer_visible:
+#                 best_match_index = df[df['username'] == username].index[0]
+#                 current_time_str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#                 df.at[best_match_index, 'last_seen'] = current_time_str
+#
+#                 socketio.emit('last_seen_update', {
+#                     'username': username,
+#                     'last_seen': current_time_str
+#                 })
+#             save_known_faces()  # Save after updating last_seen
+#
+#         visible_faces = current_visible_faces  # Update the visible faces set for the next frame
+#
+#         ret, buffer = cv2.imencode('.jpg', frame)
+#         frame = buffer.tobytes()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+#
+#
+# @app.route('/video_feed')
+# def video_feed():
+#     return Response(gen_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
+#
+#
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+#
+#
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT", 5000))
+#     socketio.run(app, host='0.0.0.0', port=port)
 from flask import Flask, render_template, Response
 from flask_socketio import SocketIO, emit
 import face_recognition
@@ -23,13 +132,13 @@ def save_known_faces():
 
 def gen_frame():
     global df
-    visible_faces = set()  # Set to keep track of faces that are currently visible
+    visible_faces = set()
 
     while True:
         _, frame = video.read()
         face_locations = face_recognition.face_locations(frame)
         face_encodings = face_recognition.face_encodings(frame, face_locations)
-        current_visible_faces = set()  # Set to keep track of faces visible in the current frame
+        current_visible_faces = set()
 
         for face_encoding, face_location in zip(face_encodings, face_locations):
             if not df.empty:
@@ -38,55 +147,51 @@ def gen_frame():
                 best_match_index = np.argmin(distances) if len(distances) > 0 else None
 
                 if best_match_index is not None and distances[best_match_index] <= 0.6:
-                    username = df.at[best_match_index, 'last_seen']
-                    current_visible_faces.add(username)  # Add to current visible faces
-
-                    if username not in visible_faces:
-                        visible_faces.add(username)  # Add to visible faces
+                    username = df.at[best_match_index, 'username']
+                    last_seen_time = df.at[best_match_index, 'last_seen']
+                    current_visible_faces.add(username)
 
                     top, right, bottom, left = face_location
                     cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
                     font = cv2.FONT_HERSHEY_DUPLEX
                     cv2.putText(frame, f'username: {username}', (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+                    cv2.putText(frame, f'last seen: {last_seen_time}', (left + 6, bottom + 15), font, 0.5,
+                                (255, 255, 255), 1)
                 else:
-                    # New face logic...
                     new_face_id = f"face_{len(df) + 1}"
                     current_time_str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                     new_row = pd.DataFrame({'username': [new_face_id], 'face_encoding': [str(face_encoding.tolist())],
                                             'last_seen': [current_time_str]})
                     df = pd.concat([df, new_row], ignore_index=True)
-                    save_known_faces()  # Save after adding a new face
+                    save_known_faces()
                     socketio.emit('last_seen_update', {
                         'username': new_face_id,
                         'last_seen': current_time_str
                     })
             else:
-                # First face logic...
                 new_face_id = f"face_1"
                 current_time_str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 df = pd.DataFrame({'username': [new_face_id], 'face_encoding': [str(face_encoding.tolist())],
                                    'last_seen': [current_time_str]})
-                save_known_faces()  # Save after adding the first face
+                save_known_faces()
                 socketio.emit('last_seen_update', {
                     'username': new_face_id,
                     'last_seen': current_time_str
                 })
 
-        # Update last_seen for faces that have left the camera's view
         faces_no_longer_visible = visible_faces - current_visible_faces
         if faces_no_longer_visible:
             for username in faces_no_longer_visible:
                 best_match_index = df[df['username'] == username].index[0]
                 current_time_str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 df.at[best_match_index, 'last_seen'] = current_time_str
-
                 socketio.emit('last_seen_update', {
                     'username': username,
                     'last_seen': current_time_str
                 })
-            save_known_faces()  # Save after updating last_seen
+            save_known_faces()
 
-        visible_faces = current_visible_faces  # Update the visible faces set for the next frame
+        visible_faces = current_visible_faces
 
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
