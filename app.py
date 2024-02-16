552d63554b3272fe9280361fcf70a0f725a29a89
Faizan <taha.rasheed@zweidevs.com>
2024-02-17 01:42:06 +0500

    added frontend

--- app.py
+++ app.py
@@ -90,5 +90,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
